21a04adc91bcd4914a194c2f7e7788b605aa2ec5
Scott Gallant <scottgallant@gmail.com>
2023-10-23 22:30:45 -0300

    added moment

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,6 +2,7 @@ import { Layout } from "../../components/Layout";
 import { useTina, tinaField } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { client } from "../../tina/__generated__/client";
+import moment from 'moment';
 
 export default function Home(props) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -11,12 +12,14 @@ export default function Home(props) {
     data: props.data,
   });
 
+  let publishDate = moment(data.post.date).format('MMM DD, YYYY');
+
   const content = data.post.body;
   return (
     <Layout>
       <article className="mx-auto w-full max-w-2xl prose-xl text-gray-600 dark:prose-invert dark:text-gray-200">
         <h1 className="text-center">{data.post.title}</h1>
-        <p className="prose-sm text-center">{data.post.date}</p>
+        <p className="prose-sm text-center">{publishDate}</p>
         <div className="font-charter" data-tina-field={tinaField(data.page, "body")}>
           <TinaMarkdown content={content} />
         </div>
